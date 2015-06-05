4cbbf546b1fe1f4c75838632ddca647c8784c48a
Yaroslav Olenenko <jaroslavo@softwareplanet.uk.com>
2015-06-05 16:29:39 +0300

    start work with test dao

---
+++ src/test/java/com/yaroslav/hotel/dao/AbstractDaoTest.java
@@ -0,0 +1,16 @@
+package com.yaroslav.hotel.dao;
+
+import com.yaroslav.hotel.config.AppConfig;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by employee on 6/5/15.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {AppConfig.class})
+@Transactional
+public abstract class AbstractDaoTest {
+}
